b35488fc292e6982eee8fc9a7e032ff1e7438e18
Tanishka saluja <tanishka.saluja07@gmail.com>
2025-12-26 15:41:10 +0530

    Update TwoSum.java

--- DSA/Arrays/TwoSum.java
+++ DSA/Arrays/TwoSum.java
@@ -1,1 +1,27 @@
+/*
+Problem: Two Sum
+Platform: LeetCode
+Approach: Use HashMap to store visited numbers and their indices.
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
 
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+}

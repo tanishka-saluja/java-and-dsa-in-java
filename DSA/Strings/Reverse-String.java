c2bbcee8d8b238745849313819053700e1e7d7f9
Tanishka saluja <tanishka.saluja07@gmail.com>
2025-12-26 15:49:54 +0530

    Update Reverse-String.java

--- DSA/Strings/Reverse-String.java
+++ DSA/Strings/Reverse-String.java
@@ -1,1 +1,19 @@
+/*
+Problem: Reverse String
+Platform: LeetCode
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+public class ReverseString {
+    public static void reverse(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}

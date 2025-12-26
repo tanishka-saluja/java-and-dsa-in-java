7b6579066434a54336dc35dd951f12eaa43e4bdd
Tanishka saluja <tanishka.saluja07@gmail.com>
2025-12-26 16:01:20 +0530

    Update isPalindrome.java

--- DSA/Strings/isPalindrome.java
+++ DSA/Strings/isPalindrome.java
@@ -1,1 +1,17 @@
+/*
+Problem: Check if String is Palindrome
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+public class IsPalindrome {
+    public static boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
